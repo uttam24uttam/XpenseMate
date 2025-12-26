73dc5d1a7e6e61ae943b05dc907e9a4971f0f77c
uttam24uttam <uttamhamsaraj24@gmail.com>
2025-12-26 20:45:13 +0530

    route update

--- Backend/Routes/friendRoute.js
+++ Backend/Routes/friendRoute.js
@@ -1,18 +1,16 @@
 import express from "express";
 import { addFriend, getFriends, getFriendDetails, getOverallBalance } from '../controllers/friendController.js';
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post('/add-friend', addFriend);
 
-router.get('/get-friends/:userId', getFriends);
+router.post('/add-friend', protect, addFriend);
 
-router.get('/get-friend-details/:friendId', getFriendDetails);
+router.get('/get-friends', protect, getFriends);
 
-router.get("/get-overall-balance/:userId", getOverallBalance);
+router.get('/get-friend-details/:friendId', protect, getFriendDetails);
+
+router.get("/get-overall-balance", protect, getOverallBalance);
 
 export default router;
-
-
-
-
