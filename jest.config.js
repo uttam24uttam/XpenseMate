500eeae5faee5d5d00f031307cf0234bfdf291b7
uttam24uttam <uttamhamsaraj24@gmail.com>
2025-12-29 16:25:39 +0530

    update

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// jest.config.js
-module.exports = {
-    transform: {
-        '^.+\\.jsx?$': 'babel-jest',
-    },
-    transformIgnorePatterns: [
-        '/node_modules/(?!axios/.*)',
-    ],
-};
